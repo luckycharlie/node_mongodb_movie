1640b554081da5275e224c8568a266c72475e206
Administrator <ccd0550@163.com>
2017-04-29 16:14:01 +0800

    gengxin

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-/**
- * Created by Administrator on 2017/4/10 0010.
- */
-var express = require("express")
-var port = process.env.PORT || 3000
-var app = express()
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.set('views',"./views")
-app.set("view engine","html")
-app.listen(port)
-console.log("port:" + port)
-app.get("/",function(req,res){
-    res.render("index",{
-        title:"电影首页"
-    })
-})
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-app.get("/",function(req,res){
-    res.render("index",{
-        title:"电影详情"
-    })
-})
+var app = express();
 
-app.get("/",function(req,res){
-    res.render("index",{
-        title:"后台录入页"
-    })
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.get("/",function(req,res){
-    res.render("index",{
-        title:"电影列表页"
-    })
-})
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
